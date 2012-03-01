f1d1a983ce9fc92de9f5cd5a7ed982be091b9506
ericmann <ericmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-01 00:14:42 +0000

    Update license terms to fix compatibility with WP.org repository.
    git-svn-id: https://plugins.svn.wordpress.org/js-banner-rotate/tags/1.2.2@512639 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jsbrotate.php
+++ jsbrotate.php
@@ -6,13 +6,15 @@ Description: Create a javascript-driven rotating banner image on your WordPress 
 Version: 1.2.2
 Author: Eric Mann
 Author URI: http://www.eamann.com
+License: GPLv2+
 */
 
 /*  Copyright 2009  Eric Mann  (email : eric@eamann.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; version 2.
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
